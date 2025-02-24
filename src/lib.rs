a864710c263291c0d746d333c2da8e505284c861
eddie <eddiebsankey@gmail.com>
2025-02-24 21:00:25 +1030

    docs: add library docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,15 @@
+//! The enum [`Language`] with variants corresponding to a specific programming language with associated
+//! file extensions and common names/aliases.
+//!
+//! [`Language`]: enum.Language.html
+//!
+//! **Crate features:**
+//!
+//! * `"serde"`
+//!   Disabled by default. Enable to `#[derive(Serialize, Deserialize)]` for `Language`
+//!
+
+#![doc(html_root_url = "https://docs.rs/lang-types/1/")]
+
 mod languages;
 pub use languages::Language;
